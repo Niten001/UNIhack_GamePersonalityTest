b27ee579ac90a785dd1d6ea5be4df05b78964c8a
Niten001 <timartin1069@gmail.com>
2018-08-05 10:55:54 +1000

    Physics engine added

--- js/client.js
+++ js/client.js
@@ -1,11 +1,49 @@
 var start = document.getElementById('start_game');
-var player = new box(20, 20, "red", 20, 20);
+var username = document.getElementById('name');
+var cont = document.getElementById('continue');
+var menu = document.getElementById('menu');
+
 var inGame = false;
 var pause = false;
-const client = io.connect('http://172.16.6.104:8000');
+var client = io();
+
+var right = false;
+var left = false;
+var jump = false;
+
+var x = 400;
+var y = 700;
 
 start.onclick = function() {
-    startGame();
+    if (username.value != "") {
+        startGame();
+    } else {
+        document.getElementById('name').style.borderBottom = "solid 2px red";
+    }
+};
+
+cont.onclick = function() {
+    if (inGame) {
+        pause = !pause;
+        pauseMenu();
+    }
+};
+
+menu.onclick = function() {
+    document.getElementById("main_menu").style.display = "block";
+    document.getElementById("logo").style.display = "block";
+    document.getElementById("name_title").style.display = "block";
+    document.getElementById("name").style.display = "block";
+    document.getElementById("start_game").style.display = "block";
+
+    if (inGame) {
+        pause = !pause;
+        pauseMenu();
+    }
+
+    inGame = false;
+    client.disconnect();
+    gameArea.canvas.parentNode.removeChild(gameArea.canvas);
 };
 
 function keyCode(ev){
@@ -16,14 +54,17 @@ window.addEventListener('keydown', function(ev) {
     switch (keyCode(ev)) {
       case "37":
         console.log("LEFT");
+        left = true;
         client.emit("input_down_event", "left");
         break;
       case "39":
         console.log("RIGHT");
+        right = true;
         client.emit("input_down_event", "right");
         break;
       case "32":
         console.log("SPACE");
+        jump = true;
         client.emit("input_down_event", "space");
         break;
     case "27":
@@ -40,14 +81,17 @@ window.addEventListener('keyup', function(ev) {
     switch (keyCode(ev)) {
         case "37":
             console.log("LEFT");
+            left = false;
             client.emit("input_up_event", "left");
             break;
         case "39":
             console.log("RIGHT");
+            right = false;
             client.emit("input_up_event", "right");
             break;
         case "32":
             console.log("SPACE");
+            jump = false;
             client.emit("input_up_event", "space");
             break;
         default:
@@ -63,6 +107,7 @@ function startGame() {
     document.getElementById("start_game").style.display = "none";
 
     inGame = true;
+    client = io.connect('http://172.16.6.104:8000');
     gameArea.start();
 
     //Connect to server
@@ -95,7 +140,7 @@ var gameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
-        this.interval = setInterval(update, 2);
+        this.interval = setInterval(render, 2);
         //Draw background
         //
         },
@@ -104,29 +149,53 @@ var gameArea = {
     }
 };
 
-function box(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.color = color;
-    this.x = x;
-    this.y = y;
-
-    this.update = function(player) {
-        this.x = player.x;
-        this.y = player.y;
-        this.width = player.width;
-        this.height = player.height;
-        this.color = player.color;
-    }
-};
-
-function update() {
+function render() {
     gameArea.clear();
     var ctx = gameArea.context;
 
-    ctx.fillStyle = player.color;
-    ctx.fillRect(player.x, player.y, player.width, player.height);
+    var image = new Image();
+    image.src = "./img/map.png";
 
+    ctx.drawImage(image, 0, 0, 1920, 1080);
+
+    //for (const player of players.values()) {
+        ctx.fillStyle = "red";//player.color;
+        ctx.fillRect(x, y, 20, 20);
+
+    //}
+    var vx = 0;
+    var vy = 0;
+    var ax = 0;
+    var ay = 5;
+
+    if ((right) && (!left)) {
+        vx = 1;
+    }
+    if ((left) && (!right)) {
+        vx = -1;
+    }
+    if (jump) {
+        ay = 2;
+        vy = -10;
+    }
+
+    vx += ax;
+    vy += ay;
+    x += vx;
+    y += vy;
+
+    if (x < 0) {
+        x = 0;
+    }
+    if (y < 0) {
+        y = 0;
+    }
+    if (x > 1884) {
+        x = 1884;
+    }
+    if (y > 760) {
+        y = 760;
+    }
 }
 
-client.on("player_update", (input) => player.update(input));
+client.on("connected", function() {console.log("TEST")});
